b4ad1cfbec619e93bd98da57d67646fdd94cf8fe
Jorge Vicuña Valle <jorge150896@hotmail.com>
2021-12-24 12:12:20 -0500

    repo prueba3

---
+++ src/pages/Error/ErrorPage.js
@@ -0,0 +1,16 @@
+import Particle from '../../components/Particle/Particle';
+
+const ErorrPage = () => {
+  return (
+    <>
+      <div className="contentHome">
+        <Particle />
+        <div className="homeSection">
+          <p>Error</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ErorrPage;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from '../components/Navbar/Navbar';
 import AboutPage from '../pages/About/AboutPage';
+import ErorrPage from '../pages/Error/ErrorPage';
 import HomePage from '../pages/Home/HomePage';
 import ProductDetail from '../pages/ProductDetail/ProductDetail';
 
@@ -13,7 +14,7 @@ export const AppRouter = () => {
         <Route path="/" exact={true} element={<HomePage />} />
         <Route path="/product/:productId" element={<ProductDetail />} />
         <Route path="/about" exact={true} element={<AboutPage />} />
-        <Route path="*" exact={true} element={<p>404</p>} />
+        <Route path="*" exact={true} element={<ErorrPage />} />
       </Routes>
     </BrowserRouter>
   );
